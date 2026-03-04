dac664c94010fd17e8f696a1e69e67fdb41d9cf3
enkeli <enkelilu@yandex.ru>
2026-03-04 12:19:12 +0300

    Fixed Deploy to GitHub Pages

---
+++ precache-manifest.f81b6b674ef31064feb491de880dce52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c258b4e8bebdcfd329a98074fcc5399",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ab6a1cd98fcca37232f",
+    "url": "./static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "c9570f2826eaf9564f89",
+    "url": "./static/js/2.6a4ef7db.chunk.js"
+  },
+  {
+    "revision": "9ab6a1cd98fcca37232f",
+    "url": "./static/js/main.1e280dee.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
